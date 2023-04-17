3e56493bc2e63593a910fc7baa27c7d094ad67ed
stevencao123 <stevencao_123@hotmail.com>
2023-04-17 13:08:01 -0700

    added list

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
 import './App.css'
-import React, { useEffect, useState } from 'react'
-import Nav from './components/Nav';
+import Nav from './components/Nav'
+import List from './components/List'
 
 
 function App() {
 
-  //const [backend, setBackend] = useState([{}])
-  /*
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackend(data)
-      }
-    )
-  }, [])
-  */
-
   return (
     <>
       <Nav />
+      <List />
     </>
   )
 }
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moment from 'moment'
+import './List.css'
 
 function List() {
-  return (
-    <div>List</div>
-  )
+
+    const [locations, setLocations] = useState([{}])
+
+    useEffect(() => {
+        fetch("/getlocations").then(
+            response => response.json()
+        ).then(
+            data => {
+                setLocations(data)
+            }
+        )
+    }, [])
+
+    return (
+        <div className='list-container'>
+            <ul className='list'>
+                {locations.map(row => (<li className='list-item' key={row.id}>{row.world}, {row.loc}, {moment(row.min).format('YYYY/MM/DD HH:mm')}, {moment(row.max).format('YYYY/MM/DD HH:mm')}, {row.founder}</li>))} 
+            </ul>
+        </div>
+    )
 }
 
 export default List
